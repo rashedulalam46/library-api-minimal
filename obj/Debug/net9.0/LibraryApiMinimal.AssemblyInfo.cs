28e98e1fe625d20020338b646cc9b2a0221957e5
Rashedul Alam <rashed464@outlook.com>
2025-11-05 13:25:17 +0600

    data file added

--- obj/Debug/net9.0/LibraryApiMinimal.AssemblyInfo.cs
+++ obj/Debug/net9.0/LibraryApiMinimal.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LibraryApiMinimal")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2f5872b5d5e14ac49c1da62e120c475b2f530a7b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c7d9fb535ad69ac07ad6a415b767430b51098f78")]
 [assembly: System.Reflection.AssemblyProductAttribute("LibraryApiMinimal")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LibraryApiMinimal")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
